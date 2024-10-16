511daf53d761b8f9790c2e5e612cfc32ac2f4d8f
Gilsonlgd <garciadasilvagilson@gmail.com>
2024-10-15 23:28:24 -0300

    ✨ feat: Implement login view

--- src/routes/publicRoutes.tsx
+++ src/routes/publicRoutes.tsx
@@ -1,5 +1,9 @@
 import Home from '@views/public/Home/Home';
+import Login from '@views/public/Login/Login';
 
-const publicRoutes = [{ path: '/', element: <Home /> }];
+const publicRoutes = [
+  { path: '/', element: <Home /> },
+  { path: '/login', element: <Login /> },
+];
 
 export default publicRoutes;
---
+++ src/views/public/Login/Login.tsx
@@ -0,0 +1,36 @@
+import './Login.scoped.css';
+
+import { useNavigate } from 'react-router-dom';
+
+import { BaseInput, BaseButton } from '@components';
+
+function Login(): JSX.Element {
+  const navigate = useNavigate();
+  return (
+    <div className="d-flex container-fluid h-100">
+      <div
+        className="container my-auto mx-auto"
+        style={{ width: '40%', maxWidth: '450px' }}
+      >
+        <div className="d-flex flex-column align-items-center gap-3">
+          <span className="text-center">
+            <h1>Bem Vindo</h1>
+            <h2>Minguard Manager</h2>
+          </span>
+          <div className="login-input-container text-center gap-3">
+            <h3>Login</h3>
+            <div>
+              <BaseInput icon="fas fa-envelope" placeholder="Email" />
+              <BaseInput icon="fas fa-lock" placeholder="Senha" />
+            </div>
+            <BaseButton onClick={() => navigate('/admin/dashboard')}>
+              <i className="fas fa-right-to-bracket" /> Entrar
+            </BaseButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
