95e85ac68ff63a164278f0581f0e9b909018dde1
Gilsonlgd <garciadasilvagilson@gmail.com>
2024-10-15 21:55:34 -0300

    🔧 refactor: Update eslint rule
    - Set 'import/no-extraneous-dependencies' eslint rule to ignore scaffolding/commands directory

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -40,6 +40,10 @@ module.exports = {
     'react/jsx-one-expression-per-line': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['src/scaffolding/commands/*'] },
+    ],
     'react/require-default-props': [
       'error',
       {
@@ -47,7 +51,7 @@ module.exports = {
       },
     ],
     'no-console': ['warn', { allow: ['error'] }],
-    'no-void': ["error", { "allowAsStatement": true }],
+    'no-void': ['error', { allowAsStatement: true }],
     'react/jsx-filename-extension': [
       1,
       {
@@ -61,8 +65,8 @@ module.exports = {
         allowExpressions: true,
       },
     ],
-    "@typescript-eslint/no-unsafe-assignment": "off",
-    "@typescript-eslint/no-unsafe-argument": "off",
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
     'import/extensions': [
       'error',
       'ignorePackages',
