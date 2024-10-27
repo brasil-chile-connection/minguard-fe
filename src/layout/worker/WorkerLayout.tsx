a9b9e5b7dd2691afec0b6381b3534a35563bfaa2
Gilsonlgd <garciadasilvagilson@gmail.com>
2024-10-27 15:41:50 -0300

    ✨ feat: Implement auth check in worker layout
    - Check if the user is authenticated or if the token is not expired
    - Redirect to the login screen if not authenticated

--- src/layout/worker/WorkerLayout.tsx
+++ src/layout/worker/WorkerLayout.tsx
@@ -1,9 +1,26 @@
+import { useEffect } from 'react';
 import './WorkerLayout.scoped.css';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
+import { toast } from 'react-toastify';
+import Auth from '@services/auth';
 import Navbar from './components/Navbar/Navbar';
 
 function WorkerLayout(): JSX.Element {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const auth = async (): Promise<void> => {
+      const isAuthenticated = await Auth.isAuthenticated();
+      if (!isAuthenticated) {
+        navigate('/');
+        toast.error('Sessão expirada. Faça login novamente.', {
+          position: 'bottom-center',
+        });
+      }
+    };
+
+    void auth();
+  }, []);
   return (
     <div style={{ height: '100%' }}>
       <Navbar />
