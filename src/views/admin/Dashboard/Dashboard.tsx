1cba84789ad88aca84f12ee6ba69fdf1dba1dc46
Gilsonlgd <garciadasilvagilson@gmail.com>
2024-11-06 22:43:14 -0300

    ✨ feat: Implement Incidents visualization for admins

--- src/views/admin/Dashboard/Dashboard.tsx
+++ src/views/admin/Dashboard/Dashboard.tsx
@@ -1,6 +1,16 @@
+import { useEffect, useState } from 'react';
 import './Dashboard.scoped.css';
 
-import { BaseTable } from '@components';
+import { useNavigate } from 'react-router-dom';
+import { scaleLinear } from 'd3-scale';
+
+import { toast } from 'react-toastify';
+import api from '@services/api';
+
+import { BaseTable, BaseButton } from '@components';
+
+import { Urgency } from '@/types/urgency';
+import { Incident } from '@/types/incident';
 
 import {
   LineChart,
@@ -24,6 +34,45 @@ import { chart1Data, chart2Data, chart3Data } from './data';
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 function Dashboard(): JSX.Element {
+  const navigate = useNavigate();
+  const colorScale = scaleLinear([0, 1, 2], ['green', 'yellow', 'red']);
+  const [incidents, setIncidents] = useState<Incident[]>([]);
+  const [urgencyLevels, setUrgencyLevels] = useState<Urgency[]>([]);
+
+  const handleLoadIncidents = async (): Promise<void> => {
+    try {
+      const { data } = await api.get<Incident[]>(`incident`);
+      setIncidents(data);
+    } catch (e) {
+      console.error(e);
+      toast.error(
+        'Não foi possível carregar a lista de incidentes. Tente novamente mais tarde.',
+        {
+          position: 'bottom-center',
+        },
+      );
+    }
+  };
+
+  const handleLoadUrgencyLevels = async (): Promise<void> => {
+    try {
+      const { data } = await api.get<Urgency[]>('/urgency');
+      setUrgencyLevels(data);
+    } catch (e) {
+      console.error(e);
+      toast.error(
+        'Erro ao carregar os dados da página. Tente novamente mais tarde.',
+        {
+          position: 'bottom-center',
+        },
+      );
+    }
+  };
+
+  useEffect(() => {
+    void handleLoadIncidents();
+    void handleLoadUrgencyLevels();
+  }, []);
   return (
     <div className="container-fluid h-100 p-3 p-md-4">
       <div className="row h-100 gap-4 gap-md-0">
@@ -33,71 +82,53 @@ function Dashboard(): JSX.Element {
             <hr />
             <BaseTable
               className="table-bordered table-hover"
-              style={{ maxHeight: '80%', overflowY: 'auto' }}
+              style={{ maxHeight: '70%', overflowY: 'auto' }}
             >
               <thead>
                 <tr className="table-light">
                   <th scope="col">#</th>
-                  <th scope="col">First</th>
-                  <th scope="col">Last</th>
-                  <th scope="col">Handle</th>
+                  <th scope="col" style={{ width: '90%' }}>
+                    Title
+                  </th>
+                  <th scope="col">Urgência</th>
+                  <th scope="col">Ações</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Pedro</td>
-                  <td>Larry the Bird</td>
-                  <td>@twitter</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Pedro</td>
-                  <td>Larry the Bird</td>
-                  <td>@twitter</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Pedro</td>
-                  <td>Larry the Bird</td>
-                  <td>@twitter</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Pedro</td>
-                  <td>Larry the Bird</td>
-                  <td>@twitter</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Pedro</td>
-                  <td>Larry the Bird</td>
-                  <td>@twitter</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Pedro</td>
-                  <td>Larry the Bird</td>
-                  <td>@twitter</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Pedro</td>
-                  <td>Larry the Bird</td>
-                  <td>@twitter</td>
-                </tr>
+                {incidents.map((incident, index) => (
+                  <tr key={incident.id}>
+                    <th scope="row">{index + 1}</th>
+                    <td>{incident.title}</td>
+                    <td>
+                      <div className="d-flex justify-content-center mt-1">
+                        <span
+                          className="urgency-badge"
+                          style={{
+                            backgroundColor: colorScale(
+                              urgencyLevels.findIndex(
+                                urg => urg.id === incident?.urgency.id,
+                              ),
+                            ),
+                          }}
+                        />{' '}
+                      </div>
+                    </td>
+                    <td>
+                      <div className="d-flex justify-content-center align-items-center">
+                        <BaseButton
+                          aria-label="Visualizar Incidente"
+                          size="sm"
+                          type="primary"
+                          onClick={() =>
+                            navigate(`/admin/incidentes/${incident.id}`)
+                          }
+                        >
+                          <i className="fas fa-eye" />
+                        </BaseButton>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </BaseTable>
           </div>
